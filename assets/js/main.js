41f270c4b2fe9751a1633a1e28b6ba8050130f60
Glorybrain <113359718+Glorybrain@users.noreply.github.com>
2024-08-12 08:35:48 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/*=============== Add Box Shadow to Header ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('navbar')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
